3e2aa0a8897aadeff423d952f2b4c8f754b4b045
Navka <navka@list.ru>
2025-01-06 03:00:44 +0300

    feat: dateSample added

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const activity = Number(sampleActivity);
+
+  if (
+    typeof sampleActivity !== "string" ||
+    isNaN(sampleActivity) ||
+    Number(sampleActivity) <= 0
+  ) {
+    return false;
+  }
+  const decayConstant = Math.LN2 / HALF_LIFE_PERIOD;
+
+  const age = Math.log(MODERN_ACTIVITY / activity) / decayConstant;
+
+  return age > 0 ? Math.ceil(age) : false;
 }
 
 module.exports = {
